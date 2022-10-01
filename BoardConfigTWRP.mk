3ad43ffb95f2aad0a884730923cca7d98c738032
Alexander Winkowski <dereference23@outlook.com>
2022-10-01 11:50:49 +0000

    citrus: Move TWRP configuration to its own .mk
    Change-Id: I7284eb22fb1ae252e955c217305afd19627028c8

---
+++ BoardConfigTWRP.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Hack: prevent anti rollback
+PLATFORM_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 16.1.0
+VENDOR_SECURITY_PATCH := 2099-12-31
+
+# UI
+TW_THEME := portrait_hdpi
+TW_EXTRA_LANGUAGES := true
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_USE_TOOLBOX := true
